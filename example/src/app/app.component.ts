4c697968a76544b65c018cda3029a357b9c6c0a9
alan buscaglia <alanbuscaglia@gmail.com>
2023-12-17 17:22:33 +0100

    fix: fix build errors

--- example/src/app/app.component.ts
+++ example/src/app/app.component.ts
@@ -2,9 +2,6 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { Componentito2Component, ComponentitoComponent } from './components';
-import { GENTLEMAN_DEFAULT_STATE } from 'gentleman-signals-state-manager';
-import { SignalsManagerService } from './services/signals-manager.service';
-import { AppSignalState } from './models/signals.model';
 
 @Component({
   selector: 'app-root',
--- example/src/app/app.config.ts
+++ example/src/app/app.config.ts
@@ -1,12 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideInitialState } from 'gentleman-signals-state-manager'
+import { provideInitialState } from 'gentleman-signals-state-manager';
 import { routes } from './app.routes';
-import { emptyAppSignalState } from './models/signals.model'
+import { emptyAppSignalState } from './models/signals.model';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideInitialState(emptyAppSignalState),
-    provideRouter(routes)
-  ]
+  providers: [provideInitialState(emptyAppSignalState), provideRouter(routes)],
 };
